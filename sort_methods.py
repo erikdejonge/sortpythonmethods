50e114dbd81880fa1528cb97ad477c692b6e8235
Erik de Jonge <erik@a8.nl>
2015-03-31 06:01:43 +0200

    sortpythonmethods Tuesday 31 March 2015 (week:13 day:90), 06:01:42

--- sort_methods.py
+++ sort_methods.py
@@ -10,6 +10,7 @@ import os
 import ast
 import inspect
 import imp
+from argparse import ArgumentParser
 
 
 def remove_breaks(source):
@@ -30,17 +31,31 @@ def remove_breaks(source):
     return lastfind, source
 
 
+def arg_parse():
+    """
+    arg_parse
+    @return: @rtype:
+    """
+    parser = ArgumentParser()
+    parser.add_argument("-m", dest="modulename", help="module name on which to sort globalmethods", required=True)
+    parser.add_argument("-w", dest="write", help="write output to file", action="store_true")
+    args = parser.parse_args()
+    return args.modulename, args.write
+
+
 def main():
     """
     main
     """
-    fname = os.path.expanduser("~") + "/workspace/pip/k8svag/k8svag/__init__.py"
-    module_name = "k8svag"
+    module_name, writefile = arg_parse()
 
+    # load module
     globals()[module_name] = __import__(module_name)
-    print(globals().keys())
 
-    
+    # get filepath of module implementation
+    fname = globals()[module_name].__file__
+
+    # get all function names on global scope
     tree = ast.parse(open(fname).read(), os.path.basename(fname), 'exec')
     names = set()
     prev = None
@@ -56,16 +71,17 @@ def main():
 
         prev = n
 
+    # sort names and request source code from module
     names = sorted(names)
-    source = open("__init__.py").read()
-    nw = open("newfile.py", "wt")
+    source = open(fname).read()
     codes = []
 
     for n in names:
-        code = "".join(inspect.getsourcelines(globals()[n])[0])
+        code = "".join(inspect.getsourcelines(getattr(globals()[module_name], n))[0])
         codes.append(code)
         source = source.replace(code, "")
 
+    # replace code block in original file, and pasted sorted functions back in
     lastfind, source = remove_breaks(source)
     first = source[:lastfind]
     last = source[lastfind:]
@@ -77,7 +93,21 @@ def main():
 
     source = first + "\n\n\n" + middle + "\n\n" + last
     lastfind, source = remove_breaks(source)
-    print(source)
+
+    # write new source
+    if writefile:
+        nw = open(fname + ".sorted", "wt")
+        nw.write(source)
+        nw.close()
+    else:
+        # try printing to terminal with syntax coloring
+        try:
+            from pygments import highlight
+            from pygments.lexers import PythonLexer
+            from pygments.formatters import TerminalFormatter
+            print(highlight(source, PythonLexer(), TerminalFormatter()))
+        except:
+            print(source)
 
 
 if __name__ == "__main__":
