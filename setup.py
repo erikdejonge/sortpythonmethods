e38f2e550c9becaf95e3e47a872955dcbb9698d2
Erik de Jonge <erik@a8.nl>
2015-04-20 08:34:06 +0200

    pip Monday 20 April 2015 (week:16 day:110), 08:34:05

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def main():
     main
     """
     setup(name='sortpythonmethods',
-          version='19',
+          version='20',
           description='Sort methods, imports and classes in a python source file',
           url='https://github.com/erikdejonge/sortpythonmethod',
           author='Erik de Jonge',
