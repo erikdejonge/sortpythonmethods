271b71a6861d2ef536141ba5df53947bd320787e
Erik de Jonge <erik@a8.nl>
2015-06-03 13:41:55 +0200

    Ludwig Mies van der Rohe: God is in the details. Wednesday 03 June 2015 (week:22 day:154), 13:41:54

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def main():
     main
     """
     setup(name='sortpythonmethods',
-          version='23',
+          version='24',
           description='Sort methods, imports and classes in a python source file',
           url='https://github.com/erikdejonge/sortpythonmethod',
           author='Erik de Jonge',
