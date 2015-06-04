453c9588f1867e03a971afb5ef0237ce15d61ddb
Erik de Jonge <erik@a8.nl>
2015-06-04 12:09:09 +0200

    Joseph Jackson: Its all about the money. Thursday 04 June 2015 (week:22 day:155), 12:09:09

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def main():
     main
     """
     setup(name='sortpythonmethods',
-          version='24',
+          version='25',
           description='Sort methods, imports and classes in a python source file',
           url='https://github.com/erikdejonge/sortpythonmethod',
           author='Erik de Jonge',
