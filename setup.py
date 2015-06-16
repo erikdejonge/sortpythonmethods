d69200e9ed15a1fce1add2f447f648b7d63e542c
Erik de Jonge <erik@a8.nl>
2015-06-16 13:18:28 +0200

    Charles de Montesquieu: True success, true happiness lies in freedom and fulfillment. Tuesday 16 June 2015 (week:24 day:167), 13:18:26

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def main():
     main
     """
     setup(name='sortpythonmethods',
-          version='27',
+          version='28',
           description='Sort methods, imports and classes in a python source file',
           url='https://github.com/erikdejonge/sortpythonmethod',
           author='Erik de Jonge',
