90868d29e6cc7db41ba2cee21cad9e16458d0bd4
Erik de Jonge <erik@a8.nl>
2015-06-24 16:20:12 +0200

    William Hazlitt: Friendship is something that is cultivated. Wednesday 24 June 2015 (week:25 day:175), 16:20:09

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def main():
     main
     """
     setup(name='sortpythonmethods',
-          version='33',
+          version='34',
           description='Sort methods, imports and classes in a python source file',
           url='https://github.com/erikdejonge/sortpythonmethod',
           author='Erik de Jonge',
