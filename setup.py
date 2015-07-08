ee061e0493888cce62560f01f2416f5fa4aff643
Erik de Jonge <erik@a8.nl>
2015-07-08 14:22:58 +0200

    Richard M. Nixon: Women are the only exploited group in history to have been idealized into powerlessness. Wednesday 08 July 2015 (week:27 day:189), 14:22:55

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def main():
     main
     """
     setup(name='sortpythonmethods',
-          version='35',
+          version='37',
           description='Sort methods, imports and classes in a python source file',
           url='https://github.com/erikdejonge/sortpythonmethod',
           author='Erik de Jonge',
